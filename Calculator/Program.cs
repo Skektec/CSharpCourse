eade0125c21c9b0cc58628f9a98b4213b6474a6b
Skekung <skekung.yt@gmail.com>
2025-11-24 12:31:10 +0200

    addition calculator added

---
+++ Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Enter a number");
+double num1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Enter another number");
+double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine($"The answer of {num1.ToString(CultureInfo.InvariantCulture)} + {num2.ToString(CultureInfo.InvariantCulture)} is {Math.Round((num1 + num2), 2).ToString(CultureInfo.InvariantCulture)}");
+Console.ReadKey();
